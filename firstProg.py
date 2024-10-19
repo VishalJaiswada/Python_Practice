dd9b30803a12e35d263094b05168402ba1952892
Vishal Jaiswada <vp022570@gmail.com>
2024-10-20 00:23:53 +0530

    Added some code in it

--- firstProg.py
+++ firstProg.py
@@ -1,1 +1,10 @@
 print("Hello Vishal Jaiswada!")
+print("He is a software Engineer,","Coder,","Problems Solver,","and a Teacher")
+
+name = "Vishal Jaiswada"
+age=23
+price=50
+
+print("My name is : ", name)
+print("My age is :",age,"years")
+print("My Salary is :",price,"LPA")
