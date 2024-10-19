44709c3854c38496c6eb0c0523fcbd6992470f3a
Vishal Jaiswada <vp022570@gmail.com>
2024-10-20 00:44:07 +0530

    Added some code

--- firstProg.py
+++ firstProg.py
@@ -3,8 +3,47 @@ print("He is a software Engineer,","Coder,","Problems Solver,","and a Teacher")
 
 name = "Vishal Jaiswada"
 age=23
-price=50
+price=50.7
 
 print("My name is : ", name)
 print("My age is :",age,"years")
 print("My Salary is :",price,"LPA")
+
+print(type(name))
+print(type(age))
+print(type(price))
+
+# Sum of two numbers
+a=10
+b=5
+sum=a+b
+print("Sum of two numbers :",sum)
+
+# difference
+diff=a-b
+print("Difference of two numbers :",diff)
+
+# product
+product = a*b
+print("Product of two numbers :",product)
+
+#division
+division =a/b
+print("Division of two numbers :",division)
+
+# Exponentiation
+
+exponentiation = a**b
+print("Exponentiation of two numbers :",exponentiation)
+
+# Operators
+"""
+Logical Operators :
+"""
+print(not False) #true
+print(not True) #false
+
+val1=True
+val2=False
+print("AND operator:", val1 and val2)
+print("OR Operator:", val1 or val2)
