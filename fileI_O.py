87ffd146cfa0f3116eac5febc9de1c6a866f5de1
Vishal Jaiswada <vp022570@gmail.com>
2024-10-21 17:10:45 +0530

    Added fileI/O

---
+++ fileI_O.py
@@ -0,0 +1,14 @@
+# File I/O in python ,Lec-7
+
+# Types of files -->
+# 1) Text files: .txt , .docx , .log etc
+# 2) Binary files: .jpg , .png , .mp4 ,.mov etc
+
+# operations are performed on the file system-->
+# Open , read & close File 
+
+# f= open("file_name","mode")
+#           |           |
+#           sample.txt   r:read Mode ,w: write mode
+#           demo.docx    
+
