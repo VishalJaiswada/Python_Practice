79df3dfe9b374684f34fa4b7152ec4901f8b1d63
Vishal Jaiswada <vp022570@gmail.com>
2024-10-24 00:38:18 +0530

    read command added

--- fileI_O.py
+++ fileI_O.py
@@ -14,7 +14,29 @@
 
 ## Coding Example
 
+# f=open("fileI_O.txt","r")
+# data=f.read()   #read entire file
+# print(data)
+# print(type(data))
+# f.close()
+
+# f=open("fileI_O.txt","r")
+# data=f.read(20) #read specified character file 
+# print(data)
+#f.close()
+
 f=open("fileI_O.txt","r")
-data=f.read()
-print(data)
-print(type(data))
+line1=f.readline() #read specified line 
+print(line1)
+
+line2=f.readline() #read specified line
+print(line2)
+
+line3=f.readline() #read specified line
+print(line3)
+
+line4=f.readline() #read specified line
+print(line4)
+
+f.close()
+ 
